0a6ba72f7fbbc94c477814ef0c5c34b5e48112c5
dottjt <julius.reade@gmail.com>
2019-10-15 15:23:04 +1100

    initial 2

--- src/scripts/saveUsers.js
+++ src/scripts/saveUsers.js
@@ -1,12 +1,16 @@
+// u - username - strings
+// l - lastContacted - 01-02-2019
+// r - replied - t or f or u
+
 // SAVE USERS WHEN SCROLLING MESSAGES
 document.addEventListener('DOMContentLoaded', function () {
   if (
     document.location.pathname.includes('message/messages') || 
     document.location.pathname.includes('message/inbox')
   ) {
-    document.addEventListener('scroll', function (event) {
+    // document.addEventListener('scroll', function (event) {
       saveUsersWhenScrollingMessages(event);
-    });
+    // });
   }
 });
 
@@ -18,7 +22,25 @@ document.addEventListener('keypress', function (event) {
 });
  
 function saveUsersWhenScrollingMessages(event) {
-  
+  try {
+    Array.from(document.querySelectorAll('.head')).forEach(function(element) {
+      const textArray = element.textContent.split(' ');
+      const username = textArray[1];
+
+      console.log(username);
+
+      if (textArray[0] === "to") { // NOTE: Will need to update the function in some way, shape or form.
+        saveUserToDatabase(newUsername, { r: 'f' });
+      } 
+      
+      if (textArray[0] === "from") {
+        // NOTE: There you go. 
+        saveUserToDatabase(newUsername, { r: 't' });
+      }
+    });
+  } catch(e) {
+    alert('saveUsersWhenScrollingMessages: ' + e);
+  }
 }
 
 function saveUserWhenSendingMessage(event) {
@@ -29,54 +51,61 @@ function saveUserWhenSendingMessage(event) {
       const inputElement = document.querySelector('[name="to"]');
       const newUsername = inputElement.value;
 
-      saveNewUserToDatabase(newUsername);
+      saveUserToDatabase(newUsername, {});
     }
   } catch(e) {
     alert('saveUserWhenSendingMessage: ' + e);
   }
 }
 
-function saveNewUserToDatabase(newUsername) {
+function saveUserToDatabase(newUsername, additionalFields) {
   chrome.storage.local.get('usersList', function (data) {
-    // console.log(JSON.stringify(data.usersList));
     if (data.usersList && data.usersList.length > 0) {
       const foundUser = data.usersList.find(user => user.username === newUsername);
 
-      // NODE: Update user if sending message AGAIN
       if (foundUser) {
-        chrome.storage.local.set({ usersList: updateUser(data.usersList, foundUser, { lastContacted: formatDate(new Date()) }) }, function () {});
+        // TODO: Will need another IF check to see if the user has been updated.
+        console.log('Existing user ' +  + ' updated');
+        chrome.storage.local.set({ usersList: updateUser(data.usersList, foundUser, { lastContacted: formatDate(new Date()) }, additionalFields) }, function () {});
       } else {
         // NOTE: Add fresh user to the database
-        chrome.storage.local.set({ usersList: data.usersList.concat(Object.assign({}, foundUser, { lastContacted: formatDate(new Date()) })) }, function () {});
+        const newUser = Object.assign({}, foundUser, { lastContacted: formatDate(new Date()) }, additionalFields);
+        console.log('New user added: ' +  + ' !');
+        chrome.storage.local.set({ usersList: data.usersList.concat(newUser) }, function () {});
       }
     } else {
       // NOTE: Initialise user list
-      chrome.storage.local.set({ usersList: [{ username: newUsername, lastContacted: formatDate(new Date()), firstContacted: formatDate(new Date()), didTheyGetBack: false }] }, function () {});        
+      chrome.storage.local.set({ usersList: [{ u: newUsername, l: formatDate(new Date()), r: 'u' }] }, function () {});        
     }
   });
 }
 
-function updateUser(usersList, userToUpdate, fieldObject) {
+function updateUser(usersList, userToUpdate, fieldObject, additionalFields) {
   return usersList.map(user => {
     if (user.username === userToUpdate.username) {
-      return Object.assign({}, userToUpdate, fieldObject);
+      return Object.assign({}, userToUpdate, fieldObject, additionalFields);
     } return user;
   });
 }
 
 function formatDate(date) {
-  var monthNames = [
-    "January", "February", "March",
-    "April", "May", "June", "July",
-    "August", "September", "October",
-    "November", "December"
-  ];
+  // var monthNames = [
+  //   "Jan", "Feb", "Mar",
+  //   "Apr", "May", "Jun", "Jul",
+  //   "Aug", "Sep", "Oct",
+  //   "Nov", "Dec"
+  // ];
 
   var day = date.getDate();
   var monthIndex = date.getMonth();
+  if (monthIndex <= 9) {
+    monthIndex = '0' + monthIndex;
+  }
+
   var year = date.getFullYear();
 
-  return day + ' ' + monthNames[monthIndex] + ' ' + year;
+  // return day + '-' + monthNames[monthIndex] + '-' + year;
+  return year + '-' + monthIndex + '-' + day;
 }
 
 
